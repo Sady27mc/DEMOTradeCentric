037799e28dfc63ebf3478b9b47e32d305a8bc452
Robert Moisecu <robertthesady@gmail.com>
2024-08-07 12:48:39 +0300

    update cy.scripts

--- cypress/e2e/radio_buttons.cy.js
+++ cypress/e2e/radio_buttons.cy.js
@@ -1,6 +1,6 @@
 describe("radio buttons", () => {
   beforeEach(() => {
-    cy.visit("https://demoqa.com/");
+    cy.visit('/');
 
     Cypress.on('uncaught:exception', (err, runnable) => {
       // returning false here prevents Cypress from failing the test
@@ -8,6 +8,7 @@ describe("radio buttons", () => {
     });
   });
 
+  // TC1 - Navigate to Radio Button section
   it('navigate to the Radio Button section', () => {
     // Navigate to the Elements section
     cy.get('.category-cards > :nth-child(1)').should('contain', 'Elements').click();
@@ -16,9 +17,11 @@ describe("radio buttons", () => {
     cy.get('.element-list.collapse.show > ul > :nth-child(3)').should('contain', 'Radio Button').click();
 
     // Assert that the URL is correct after navigation
-    cy.url().should('eq', 'https://demoqa.com/radio-button'); 
+    cy.url().should('eq', Cypress.config('baseUrl') + '/radio-button'); 
   });
 
+
+  // TC2 - Select Yes button
   it('select Yes radio button', () => {
     // Navigate to the Elements section
     cy.get('.category-cards > :nth-child(1)').should('contain', 'Elements').click();
@@ -31,9 +34,11 @@ describe("radio buttons", () => {
     cy.get('.text-success').should('contain', 'Yes');
 
     // Assert that the URL is correct after navigation
-    cy.url().should('eq', 'https://demoqa.com/radio-button'); 
+    cy.url().should('eq', Cypress.config('baseUrl') + '/radio-button'); 
   });
 
+
+  // TC3 - Select Impressive button
   it('select Impressive radio button', () => {
     // Navigate to the Elements section
     cy.get('.category-cards > :nth-child(1)').should('contain', 'Elements').click();
@@ -46,7 +51,7 @@ describe("radio buttons", () => {
     cy.get('.text-success').should('contain', 'Impressive');
 
     // Assert that the URL is correct after navigation
-    cy.url().should('eq', 'https://demoqa.com/radio-button');    
+    cy.url().should('eq', Cypress.config('baseUrl') + '/radio-button');    
   });
 });
 
--- cypress/e2e/radio_buttonsDRY.cy.js
+++ cypress/e2e/radio_buttonsDRY.cy.js
@@ -1,6 +1,6 @@
 describe("radio buttons", () => {
     beforeEach(() => {
-      cy.visit("https://demoqa.com/");
+      cy.visit("/");
 
       Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from failing the test
@@ -14,22 +14,28 @@ describe("radio buttons", () => {
       // Navigate to the Radio Button menu
       cy.get('.element-list.collapse.show > ul > :nth-child(3)').should('contain', 'Radio Button').click();
           // Assert that the URL is correct after navigation
-      cy.url().should('eq', 'https://demoqa.com/radio-button'); 
+      cy.url().should('eq', Cypress.config('baseUrl') + '/radio-button'); 
     };
   
     const selectRadioButton = (radioButtonId, expectedText) => {
       cy.get(radioButtonId).check({ force: true });
       cy.get('.text-success').should('contain', expectedText);
     };
+
+
   //Test Case 1 - Navigate to the Radio Button Page and assert the URL
     it('navigate to the Radio Button section', () => {
       navigateToRadioButtonSection();
     });
+
+
   //Test Case 2 - Navigate to the Radio Button Page, assert the URL and select and assert option Yes
     it('select Yes radio button', () => {
       navigateToRadioButtonSection();
       selectRadioButton('#yesRadio', 'Yes');
     });
+
+    
   //Test Case 3 - Navigate to the Radio Button Page, assert the URL and select and assert option Impressive
     it('select Impressive radio button', () => {
       navigateToRadioButtonSection();
